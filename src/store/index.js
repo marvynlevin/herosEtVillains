91525a57c7bd6082bab0c88be753b44902442672
lynn2910 <cedic2005@gmail.com>
2025-03-20 10:11:01 +0100

    feat: Change authentification method to use headers

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,10 @@ import Vuex from "vuex";
 import hero from './hero.store';
 import team from './team.store';
 import org from './org.store';
+import error from './error.store'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    hero: hero,
-    team: team,
-    org: org,
-  },
+    modules: {hero, team, org, error},
 });

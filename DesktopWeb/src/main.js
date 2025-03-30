a0a6f8d210f3bc46a8206c413e757fad25a7ab91
lynn2910 <cedric.colin35@gmail.com>
2025-03-30 22:20:16 +0200

    RANDOM BULLSHIT GO

--- DesktopWeb/src/main.js
+++ DesktopWeb/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store/index.js';
-import router from './router/index.js';
+import store from './store';
+import router from './router';
 import Vuetify from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/dist/vuetify.min.css'
@@ -11,8 +11,8 @@ Vue.config.productionTip = false;
 Vue.use(Vuetify);
 
 new Vue({
-  store,
-  router,
-  vuetify: new Vuetify(),
-  render: h => h(App)
+    store,
+    router,
+    vuetify: new Vuetify(),
+    render: h => h(App)
 }).$mount('#app');

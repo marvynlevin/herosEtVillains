69f42827aaa1523488d5603075e09802887bd3e9
lynn2910 <cedric.colin35@gmail.com>
2025-03-31 11:14:22 +0200

    feat(mobile): 404 page

--- MobileVue3/src/router/index.ts
+++ MobileVue3/src/router/index.ts
@@ -5,6 +5,7 @@ import AuthComponent from "@/views/AuthComponent.vue";
 import UserAuthComponent from "@/views/UserAuthComponent.vue";
 import {orgRoutes} from "@/router/org.router";
 import UserRegisterComponent from "@/views/UserRegisterComponent.vue";
+import NotFoundComponent from "@/views/NotFoundComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -31,7 +32,12 @@ const routes: Array<RouteRecordRaw> = [
         name: 'Register',
         component: UserRegisterComponent
     },
-    ...orgRoutes
+    ...orgRoutes,
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: NotFoundComponent,
+    },
 ]
 
 const router = createRouter({
---
+++ MobileVue3/src/views/NotFoundComponent.vue
@@ -0,0 +1,62 @@
+<template>
+	<ion-page>
+		<ion-content>
+			<div class="not-found">
+				<h1>404 - Page Not Found</h1>
+				<p>Sorry, the page you are looking for does not exist.</p>
+				<router-link to="/">Go to Homepage</router-link>
+			</div>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {IonPage, IonContent} from '@ionic/vue';
+import {useRouter} from 'vue-router';
+
+export default defineComponent({
+	name: 'NotFoundComponent',
+	components: {
+		IonPage,
+		IonContent,
+	},
+	setup() {
+		const router = useRouter();
+
+		const goToHomePage = () => {
+			router.push('/');
+		};
+
+		return {
+			goToHomePage,
+		};
+	},
+});
+</script>
+
+<style scoped>
+.not-found {
+	text-align: center;
+	margin-top: 100px;
+}
+
+.not-found h1 {
+	font-size: 4rem;
+	color: #ff4d4f;
+}
+
+.not-found p {
+	font-size: 1.2rem;
+}
+
+.not-found a {
+	font-size: 1.1rem;
+	color: #1890ff;
+	text-decoration: none;
+}
+
+.not-found a:hover {
+	text-decoration: underline;
+}
+</style>
